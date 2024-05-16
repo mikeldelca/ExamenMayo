d1d27cacfe31fe9fc625292579db717bddd224f2
Alumno <Alumno@AULA1-PC23>
2024-05-16 16:37:28 +0200

    Examen de mayo, entorno de desarrollo

---
+++ src/Principal.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		String nombre;
+		Scanner elegir=new Scanner(System.in);
+		
+		System.out.println("----Saludos, podias indicar tu nombre----");
+		nombre=elegir.nextLine();
+		System.out.println("***** HOLA "+nombre+" , perteneces a la generacion de futuro *****");
+		
+	
+
+	}
+
+}
